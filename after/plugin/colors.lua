f17fd44091a4a18d7763d1f74479f82c9d1bf8fa
janex <janexpl@me.com>
2022-12-21 22:26:25 +0100

    colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,17 @@
-function ColorMyPencils(color)
-	color = color or "catppuccin-frappe"
-	vim.cmd.colorscheme(color)
+local status, clr = pcall(require, "catppuccin")
+if (not status) then return end
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+clr.setup({
+  flavour = "frappe",
+  transparent_background = true
+})
+
+function ColorMyPencils(color)
+  color = color or "catppuccin"
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
